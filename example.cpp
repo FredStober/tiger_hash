db20b7ba23862828764b36c1bf28f838ef790a2b
Fred Stober <stober@cern.ch>
2016-11-05 09:45:54 +0100

    improved the example and the description in the readme file

--- example.cpp
+++ example.cpp
@@ -3,7 +3,19 @@
 
 int main()
 {
+	// 1) Initialize the algorithm
+	Hash::tiger_algorithm_t tiger;
+	// 2) Update the internal state
+	tiger.update(u8"Hallo Welt");
+	// 3) Get the hash from the algorithm (this also resets the internal state of the algorithm!)
+	Hash::tiger_hash_t hash = tiger.get_digest();
+	// 4) Display the hash in hexadecimal format
+	std::cout << Hash::to_string(hash) << std::endl;
+
+	// *) Hash calculation in one step:
+	std::cout << Hash::to_string(Hash::tiger2_algorithm_t(u8"Hallo Welt").get_digest()) << std::endl;
 	Hash::tiger_algorithm_t tiger;
 	tiger.update(u8"Hallo Welt");
-	std::cout << Hash::to_string(tiger.get_digest()) << std::endl;
+	Hash::tiger_hash_t hash = tiger.get_digest();
+	std::cout << Hash::to_string(hash) << std::endl;
 }
